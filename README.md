d906cf9246fb905cb492a48da4dae3e9461ae2d5
Sev <captain.sev@outlook.com>
2023-12-06 12:02:57 +0100

    restructured for npm

--- README.md
+++ README.md
@@ -1,9 +1,105 @@
+# Leaflet Outline Connector
+
 Leaflet connectors is a plugin to draw lines from the outlines of various geometries.
 
-
 ## Supported Geometries
+
+Currently the following geometries are supported:
 
 + CircleMarker
 + Circle
 + ShapeMarkers
     + Square
+
+## Installation
+
+Using yarn
+```cmd
+yarn add leaflet-connector
+```
+
+Using npm
+```
+npm i leaflet-connector
+```
+
+## Import into your environment
+
+You can import the plugin into your environment,
+with the following methods:
+
+### Script Tag
+
+```HTML
+<!-- Import leaflet first and then, ... -->
+<script src="/node_modules/leaflet-outline-connector/leaflet-outline-connector.js"></script>
+```
+
+### Import Statement
+```js
+    import L from "leaflet"
+    // You can just import the plugin,
+    // it will be automatically available on 
+    // the 'L' object.
+    import "leaflet-outline-connector"
+```
+
+## Usage
+
+### Simple Connector
+
+When having two (or more) that you want to connect, you 
+simply create a connector like that:
+
+```js
+        /*
+         < setup leaflet regulary with a map variable called 'map' > 
+        */
+
+        // You can even connect CIRCLE MARKERS ...
+        const Stuttgart = L.circleMarker([48.7784485, 9.1800132]).addTo(map);
+
+        // with CIRCLES.
+        const Hamburg = L.circle([53.550341, 10.000654], { radius: 20000}).addTo(map);
+
+        // OPTIONAL: You can provide your own options to style the path...
+        let options = {
+            color: "red"
+        }
+
+        //          ... or use the options of one of the existing geometries.
+        options = Stuttgart.options
+
+
+        L.connector([Stuttgart, Hamburg], options)
+                // The connector is a normal path object that must be added
+                // to the map separately from the markers.
+                .addTo(map)
+                // Optional to bring the connector to the back
+                // otherwise the line end is on top of the markers
+                // and it looks not so clean.
+                .bringToBack()
+```
+
+## Connect one (origin) with many (destination)
+
+You may also provide as many elements as you want, whereas the first element is the origin and for each further element (destination) a line is created from the origin to that destination. 
+
+```js
+        // Multiple example
+
+        const franceColor = "blue";
+
+        const Paris_origin = L.circle([48.788771, 2.323608], { radius: 30000, fill: false, color: franceColor }).addTo(map);
+        const Reims = L.shapeMarker([49.2595, 4.02798], { shape: "square", radius: 10, fill: false, color: franceColor }).addTo(map);
+        const Rouen = L.circle([49.434347, 1.0943916], { radius: 10000, fill: false, color: franceColor }).addTo(map);
+        const LeMans = L.circleMarker([47.982456, 0.218905], { radius: 10, fill: false, color: franceColor }).addTo(map);
+
+        /**
+         * When connecting multiple geometries, the first 
+         * provided element is the origin and all connectors
+         * will be drawn from this element to the others.
+         */
+        L.connector([Paris_origin, Reims, Rouen, LeMans], { color: franceColor }).addTo(map);
+```
+
---
+++ leaflet-outline-connector.js
@@ -0,0 +1,252 @@
+(function (factory, window) {
+
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
+
+        // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('leaflet'));
+    }
+
+    // attach your plugin to the global 'L' variable
+    if (typeof window !== 'undefined' && window.L) {
+        window.L.Connector = factory(L);
+    }
+}(function (L) {
+
+    /**
+     * @class
+     * @classdesc Represents a connector between multiple geometries on a Leaflet map.
+     * @extends L.Path
+     * @param {Array} points - An array of geometries representing the points of the connector.
+     * @param {Object} options - Options for the connector.
+     * @throws {Error} Throws an error if there are not enough points in the array.
+     */
+    L.Connector = L.Path.extend({
+
+        initialize: function (points, options) {
+            if (points.length < 2) {
+                throw new Error('Not enough points')
+            }
+            L.setOptions(this, options);
+            this._createAnchors(...points);
+        },
+
+
+
+        _createAnchors(...geom) {
+            this._anchors = [];
+            for (let i = 0; i < geom.length; i++) {
+                const type = this.checkGeometry(geom[i])
+                const anchor = {
+                    isOrigin: i === 0,
+                    type: type,
+                    geom: geom[i],
+                    vector: null,
+                    normalizedVector: null
+                }
+
+                if (i > 0) {
+                    this._calculateAnchorVector(anchor)
+                }
+
+                this._anchors.push(anchor)
+            }
+        },
+
+        _getOriginAnchor() {
+            return this._anchors[0]
+        },
+
+        _calculateAnchorVector(anchor) {
+            const origin = this._getCenter(this._getOriginAnchor())
+            const anchorCenter = this._getCenter(anchor)
+            anchor.vector = this._vectorFromPoints(origin, anchorCenter)
+            anchor.normalizedVector = this._normalizeVector(anchor.vector)
+        },
+
+        _vectorFromPoints(point1, point2) {
+            if (point1.x === point2.x && point1.y === point2.y) {
+                return L.point(1, 1)
+            }
+
+            return L.point(point1.x - point2.x, point1.y - point2.y)
+        },
+
+        _normalizeVector(vector) {
+            const length = vector.distanceTo(L.point(0, 0))
+            return L.point(vector.x / length, vector.y / length)
+        },
+
+        flipVector(vector) {
+            return L.point(-vector.x, -vector.y)
+        },
+
+
+        _getCenter(anchor) {
+            switch (anchor.type) {
+                case 'circle':  // fallthrough
+                case 'circle-marker': // fallthrough
+                case 'rect': return anchor.geom._point
+                default: throw new Error('Unsupported geometry type')
+            }
+        },
+
+        /**
+         * Checks the type of geometry and returns the corresponding type.
+         * @param {L.Path} geom - The path-like object to check.
+         * @returns {string} - The type of geometry (currently supported: 'rect', 'circle', 'circle-marker').
+         * @throws {Error} - If the geometry type is not supported.
+         */
+        checkGeometry(geom) {
+            let type = null
+            if (this._isShapeMarkerSquare(geom)) {
+                type = 'rect'
+            } else if (geom instanceof L.Circle) {
+                type = 'circle'
+            } else if (geom instanceof L.CircleMarker) {
+                type = 'circle-marker'
+            } else {
+                throw new Error('Unsupported geometry type')
+            }
+            return type
+        },
+
+        _project() {
+            this._update();
+        },
+
+        _update() {
+            if (this._map) {
+                this._updatePath();
+            }
+        },
+
+        _updatePath() {
+            let lines = []
+
+            for (let anchor of this._anchors) {
+                if (anchor.isOrigin) continue
+                let reversedNormalizedVector = L.point(-anchor.normalizedVector.x, -anchor.normalizedVector.y)
+                let originAnchor = Object.assign({}, this._getOriginAnchor(), { normalizedVector: reversedNormalizedVector })
+
+                const anchorPointOnGeometry = this._getPointFromAnchor(anchor, originAnchor)
+                const originPointOnGeometry = this._getPointFromAnchor(originAnchor, anchor)
+                lines.push([originPointOnGeometry, anchorPointOnGeometry])
+            }
+
+            this._parts = lines;
+            this._renderer._updatePoly(this);
+        },
+
+        _getPointFromAnchor(anchor, other) {
+            let point = null
+            if (anchor.type === 'circle') {
+                point = this._getPointOnCircle(anchor)
+            } else if (anchor.type === 'circle-marker') {
+                point = this._getPointOnCircleMarker(anchor)
+            } else if (anchor.type === 'rect') {
+                point = this._getPointOnRect(anchor, other)
+            } else {
+                throw new Error('Unsupported geometry type')
+            }
+
+            return point
+        },
+
+        _getPointOnCircle(anchor) {
+            let directionVector = anchor.normalizedVector
+            return anchor.geom._point.add(directionVector.multiplyBy(anchor.geom._radius))
+        },
+
+        _getPointOnCircleMarker(anchor) {
+            let directionVector = anchor.normalizedVector
+            return anchor.geom._point.add(directionVector.multiplyBy(anchor.geom.options.radius))
+        },
+
+
+        _getPointOnRect(anchor, other) {
+            // Calculate intersection for point with all four sides of the rectangle
+            let intersections = []
+            const horizontal = new L.Point(1, 0)
+            const vertical = new L.Point(0, 1)
+
+            const size = anchor.geom.options.radius
+
+            let borders = [
+                { name: "Bottom", point: this._getCenter(anchor).add(vertical.multiplyBy(size)).subtract(horizontal.multiplyBy(size)), vector: horizontal },
+                { name: "Right", point: this._getCenter(anchor).add(horizontal.multiplyBy(size)).subtract(vertical.multiplyBy(size)), vector: vertical },
+                { name: "Top", point: this._getCenter(anchor).add(vertical.multiplyBy(-size)).subtract(horizontal.multiplyBy(size)), vector: horizontal },
+                { name: "Left", point: this._getCenter(anchor).add(horizontal.multiplyBy(-size)).subtract(vertical.multiplyBy(size)), vector: vertical }
+            ]
+
+            let closest = { point: null, distance: Infinity }
+
+            borders.forEach(({ name, point, vector } = {}) => {
+                let { point: intersectionPoint, distance } = this._getIntersection(point, vector, size, other)
+
+                if (distance < closest.distance) {
+                    closest = { point: intersectionPoint, distance }
+                }
+            })
+
+            return closest.point
+        },
+
+        _getIntersection(borderStartPoint, borderVector, size, anchor) {
+
+            const originAnchor = this._getOriginAnchor()
+            const originAnchorPoint = this._getCenter(originAnchor)
+
+            let lineStart = borderStartPoint
+            let lineEnd = borderStartPoint.add(borderVector.multiplyBy(size * 2))
+
+            let line2Start = originAnchorPoint
+            let line2End = originAnchorPoint.add(this.flipVector(anchor.normalizedVector).multiplyBy(size * 2))
+
+            // Calculate if the lines interesct at all
+            let denominator = ((lineEnd.x - lineStart.x) * (line2End.y - line2Start.y)) - ((lineEnd.y - lineStart.y) * (line2End.x - line2Start.x))
+
+            // Break if lines are parallel
+            if (denominator === 0) {
+                return { point: null, distance: Infinity }
+            }
+
+            let a = lineStart.y - line2Start.y
+            let b = lineStart.x - line2Start.x
+            let numerator1 = ((line2End.x - line2Start.x) * a) - ((line2End.y - line2Start.y) * b)
+            let numerator2 = ((lineEnd.x - lineStart.x) * a) - ((lineEnd.y - lineStart.y) * b)
+            a = numerator1 / denominator
+            b = numerator2 / denominator
+
+            // if we cast these lines infinitely in both directions, they intersect here:
+            let x = lineStart.x + (a * (lineEnd.x - lineStart.x))
+            let y = lineStart.y + (a * (lineEnd.y - lineStart.y))
+
+
+            if (x < Math.min(lineStart.x, lineEnd.x) || x > Math.max(lineStart.x, lineEnd.x) ||
+                y < Math.min(lineStart.y, lineEnd.y) || y > Math.max(lineStart.y, lineEnd.y)) {
+                return { point: null, distance: Infinity }
+            }
+
+            const anchorPoint = this._getCenter(anchor)
+
+            return { point: new L.Point(x, y), distance: anchorPoint.distanceTo(new L.Point(x, y)) }
+        },
+
+        _isShapeMarker(geom) {
+            return geom instanceof L.ShapeMarker
+        },
+
+        _isShapeMarkerSquare(geom) {
+            return this._isShapeMarker(geom) && geom.options.shape === 'square'
+        }
+    });
+
+    L.connector = function (a, b, options) {
+        return new L.Connector(a, b, options);
+    };
+
+    return L.Connector;
+}, window));
